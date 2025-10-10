c9574aa906e7710cdea7136c5f405c4a62aca633
z1w0k <1zWkys@gmail.com>
2025-10-10 21:51:32 +0300

    Был доделан task2

--- task2/task2_2.c
+++ task2/task2_2.c
@@ -4,22 +4,22 @@
 
 int main() {
 	double x, coeff, result;
-
+    double result_polynomial = 0.0; 
+    double result_derivative = 0.0;
 
 	if(scanf("%lf", &x) != 1) {
 		return 1;
-	}
+    }
 
-
-	if(scanf("%lf", &result) != 1) {
-		return 1;
-	}
 
 	while (scanf("%lf", &coeff) != EOF) {
-		result = result*x + coeff;
+        result_derivative = result_derivative * x + result_polynomial;   
+		result_polynomial = result_polynomial * x + coeff;
+
 	} 
 
-	printf("%lf\n", result);
+	printf("Многочлен = %.2lf\n", result_polynomial);
+    printf("Его производная = %.2lf\n", result_derivative);
 
 	return 0;
 }
